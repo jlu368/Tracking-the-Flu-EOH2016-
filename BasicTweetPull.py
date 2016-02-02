b431e6cb3038170758d806d4dd818c42d7302d43
Jeff L <j.lu368@gmail.com>
2016-02-02 16:35:24 -0600

    New Commit
    Adding new files

--- BasicTweetPull.py
+++ BasicTweetPull.py
@@ -20,7 +20,7 @@ def print_tweets(api, filter, start_date, end_date, region, count):
             #if (datetime.datetime.now() - tweet.created_at).days < 5: # example time stamp: 2015-11-24 02:13:33 or 2015-11-01
             #    #Do processing here:
             #    #print tweet.geocode
-            print(tweet.text.encode("utf-8"))
+            print tweet.text.encode("utf-8")
             ct = ct + 1
 
             # exit clause
@@ -50,7 +50,7 @@ def write_tweets_to_json(api, filter, start_date, end_date, region, count, json_
             #    #Do processing here:
             #    #print tweet.geocode
 
-            list_of_tweets.append(str(tweet.text.encode("utf-8")))
+            list_of_tweets.append(tweet.text.encode("utf-8"))
 
             #with open('tweets.json', 'a') as f:
             #     json.dump(tweet.text.encode("utf-8"), f)
@@ -58,6 +58,7 @@ def write_tweets_to_json(api, filter, start_date, end_date, region, count, json_
 
             # exit clause
             if ct == count:
+                print('three')
                 with open(json_file, 'w') as f:
                      json.dump(list_of_tweets, f)
                 return
@@ -69,7 +70,7 @@ def write_tweets_to_json(api, filter, start_date, end_date, region, count, json_
             time.sleep(500)
 
 
-def main():
+def tweetPull():
 
     # API stuff
     access_token = "97325888-4TiheE2EDlLRrRd0i3RyL99uIRMIjqOgMJilVWxsO"
@@ -84,12 +85,12 @@ def main():
     file = open("filter.txt") # read in filter words from file named "filter.txt"; alter that file or just write it in here
     filter = file.readlines()
     file.close()
-    for x in range(0, len(filter)):
+    for x in xrange(0, len(filter)):
         filter[x] = filter[x].strip()
 
     # dates to search within
-    start_date = "2015-11-01" # year-mo-dy format
-    end_date = "2016-01-20"
+    start_date = "2015-12-01" # year-mo-dy format
+    end_date = "2016-01-01"
 
     # area of search
     region = [41.65,-79.67,44.91,-73.39] # set using coordinates (I think this is the state of New York atm)
@@ -102,10 +103,9 @@ def main():
 
     ''' run function '''
     #print_tweets(api, filter, start_date, end_date, region, count)
-    write_tweets_to_json(api, filter, start_date, end_date, region, count, 'tweets.json')
-    return
+    write_tweets_to_json(api, filter, start_date, end_date, region, count, json_file)
 
-main()
+tweetPull()
 
 # Be aware that the program will not end after displaying the desired set of tweets at the moment.
 # You will need to Ctrl-C or whatever manually for the time being
